1fed0efadce4721c69eebf38eff178f160d5bbcf
jwong2529 <janice.wong2529@gmail.com>
2026-01-29 22:46:24 -0500

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-4xl font-bold text-black dark:text-white">
+          Hello World 😊
+        </h1>
+      </main>
+    </div>
+  );
+}
